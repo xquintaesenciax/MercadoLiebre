0bdb9b02096398eab783e562dc71b1c34dad7591
xquintaesenciax <nuria.b.t.r@gmail.com>
2023-11-02 19:41:25 -0300

    formulario de registro añadido, faltan los estilos

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/views/home.html")
 })
 
+app.get("/register", (req, res)=>{
+    res.sendFile(__dirname + "/views/register.html")
+})
+
 
 app.listen(3000, ()=> {
     console.log("Servidor corriendo")
